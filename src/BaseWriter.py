b987c82b90b4b64d698b555f5d444a2c87778d6a
p-zander <11837635+p-zander@users.noreply.github.com>
2025-08-01 23:37:18 +0200

    Support delimeter config for CSV; Improve Metadata printing; Rename special longer format; Improvements

--- src/BaseWriter.py
+++ src/BaseWriter.py
@@ -1,13 +1,14 @@
 from abc import ABC, abstractmethod
-from os import linesep
-from typing import List
+from os import linesep, path
+from typing import Any, List, Tuple
 
 from robot.api import SuiteVisitor
 from robot.model import TestCase
 from robot.utils import robottime
 
+
 class BaseWriter(SuiteVisitor, ABC):
-    default_format = (
+    default_format: Tuple[str] = (
         "full_name",
         "doc",
         "tags",
@@ -17,8 +18,8 @@ class BaseWriter(SuiteVisitor, ABC):
         "starttime",
         "endtime",
     )
-    default_format_str = ",".join(default_format)
-    more_format = (
+    default_format_str: str = ",".join(default_format)
+    long_format: Tuple[str] = (
         "id",
         "parent.name",
         "name",
@@ -28,63 +29,74 @@ class BaseWriter(SuiteVisitor, ABC):
         "source",
         "lineno",
     )
-    more_format_str = ",".join(more_format)
+    long_format_str: str = ",".join(long_format)
+    default_docs_max_len: int = 100
 
-    def __init__(
-        self,
-        output_file: str,
-        format_specifier: str = default_format_str,
-        docs_max_len: int = 100,
-    ):
-        self.output_file, self.docs_max_len = output_file, docs_max_len
-        self.format_specifier = (
-            BaseWriter.more_format_str
-            if format_specifier == "MORE"
-            else format_specifier
-        )
+    def __init__(self, output_filename: str, format_specifier: str = None, docs_max_len: Any = None) -> None:
+        if not format_specifier:
+            self.format_specifier = BaseWriter.default_format_str
+        elif format_specifier == "LONG":  # Special case
+            self.format_specifier = BaseWriter.long_format_str
+        else:
+            self.format_specifier = format_specifier
 
-        self.open_output_file(self.output_file)
-        self.write_header(self.format_specifier)
+        self.docs_max_len = int(docs_max_len) if docs_max_len else BaseWriter.default_docs_max_len
 
-    def visit_test(self, test: TestCase):
+        self.open_output_file(output_filename)
+        self.write_header_pretty(self.format_specifier)
+
+    def visit_test(self, test: TestCase) -> None:
         data: List[str] = list()
 
         for item in self.format_specifier.split(","):
             match item.lower():
-                case x if "parent" in x:
-                    value = str(getattr(test.parent, item.split(".")[1]))
+                case "parent.metadata":
+                    meta = str([list(test.parent.metadata.items())])
+                    value = "" if meta == "[[]]" else meta[1:-2]
+                case i if "parent" in i:
+                    value = getattr(test.parent, i.split(".")[1])
                 case "doc":
                     value = test.doc[: self.docs_max_len]
                 case "shortdoc":
-                    value = test.doc
-                    value = value[: value.find(linesep)]
+                    doc = test.doc[: self.docs_max_len]
+                    value = doc[: doc.find(linesep)]
                 case "tags":
-                    value = ",".join(test.tags)
+                    value = ", ".join(test.tags)
                 case "elapsedtime":
-                    value = robottime.secs_to_timestr(test.elapsedtime/1000, compact=True)
+                    value = robottime.secs_to_timestr(test.elapsedtime / 1000, compact=True)
                 case i:
-                    value = str(getattr(test, i))
-            data.append(value)
+                    value = getattr(test, i)
+            data.append(str(value))
 
         self.write_data(data)
 
-    def write_header(self, format_specifier: str):
-        data = [self.pretty_name(entry) for entry in format_specifier.split(",")]
-        self.write_data(data)
+    def write_header_pretty(self, format_specifier: str) -> None:
+        data = [self.__pretty_name(entry) for entry in format_specifier.split(",")]
+        self.write_header(data)
 
-    def pretty_name(self, specifier: str):
+    def __pretty_name(self, specifier: str) -> str:
         my_pretty_dict = {
             "doc": "Documentation",
-            "starttime": "Start Time",
-            "endtime": "End Time",
             "elapsedtime": "Elapsed Time",
+            "endtime": "End Time",
             "lineno": "Line",
+            "name": "Test Name",
+            "parent.metadata": "Suite Metadata",
+            "parent.name": "Suite Name",
+            "starttime": "Start Time",
         }
         default_pretty_name = specifier.replace("_", " ").replace(".", " ").title()
         return my_pretty_dict.get(specifier, default_pretty_name)
 
-    @abstractmethod
-    def open_output_file(self, output_file: str): ...
+    def print_success(self, filename: str):
+        filepath = path.abspath(path.join(path.curdir, filename))
+        print(f"Tabular: {filepath}")
 
     @abstractmethod
-    def write_data(self, data: list): ...
+    def open_output_file(self, output_filename: str) -> None: ...
+
+    @abstractmethod
+    def write_data(self, data: List[str]) -> None: ...
+
+    @abstractmethod
+    def write_header(self, data: List[str]) -> None: ...
